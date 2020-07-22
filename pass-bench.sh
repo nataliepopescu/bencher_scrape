cada4854c975aafc4b567a88276767c5aabe4b06
npopescu <npopescu@localhost>
2020-07-22 17:04:23 -0500

    linking now works on minimal crate w dummy pass

--- pass-bench.sh
+++ pass-bench.sh
@@ -4,16 +4,29 @@
 
 set -x
 
-NAME="example"
-DIR=`pwd`/$NAME
+ROOT="$PWD"
 LLVM_HOME=/benchdata/llvm-project/build
 RUSTUP_TOOLCHAIN_LIB=/benchdata/.rustup/toolchains/nightly-2020-05-07-x86_64-unknown-linux-gnu/lib/rustlib/x86_64-unknown-linux-gnu/lib
+NOPIE_SCRIPT="$ROOT/make_no_pie.py"
+
+DIR="$ROOT/example"
+OUTDIR="$DIR/target/debug/deps"
+LINKARGS="$DIR/link-args"
 
 # Build main with temporary files preserved and emit LLVM-IR
+# Also use cargo's '-Z print-link-args' to get the exact linker command
 cd $DIR
 cargo clean
-cargo rustc --verbose -- -Z print-link-args -v -C save-temps --emit=llvm-ir > "linked"
-cd target/debug/deps
+cargo rustc --verbose -- -Z print-link-args -v -C save-temps --emit=llvm-ir > "$LINKARGS"
+
+# Replace instances of "-pie" with "-no-pie", otherwise get 
+# "relocation R_X86_64_32 against `.rodata' can not be used when 
+# making a PIE object; recompile with -fPIE" error
+OUT="tmp"
+python3 "$NOPIE_SCRIPT" "$LINKARGS" "$OUT"
+mv "$OUT" "$LINKARGS"
+
+cd $OUTDIR
 
 # Remove the unoptimized bc or we'll get duplicate symbols at link time
 rm *no-opt*
@@ -27,15 +40,7 @@ find . -name '*.bc' | rev | cut -c 3- | rev | xargs -n 1 -I {} $LLVM_HOME/bin/op
 # Compile the bitcode to object files
 find . -name '*.bc' | xargs -n 1 $LLVM_HOME/bin/llc -filetype=obj
 
-# Complete the linking to default a.out file
-# Extra flags for removing C++ default libs, but link System, resolv, libc, and math.
-# Also strip dead code so we don't have tons of rust std library code that isn't referenced.
-# Use cargo's '-Z print-link-args' to get the exact set of .rlibs
+# Complete the linking with previously saved/process command
+/bin/bash $LINKARGS
 
-#while read -r line
-#do
-#done < $OUTPUT_DIR/file-names
-
-#clang -m64 *.o -Wl,--start-group $(find $RUSTUP_TOOLCHAIN_LIB -name '*rlib') -Wl,--end-group -lpthread -ldl
-
-`cat /benchdata/rust/bencher_scrape/example/linked`
+cd $ROOT

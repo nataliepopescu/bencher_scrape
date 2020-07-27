9863b719e636b34a2683a88fcadf16f250818d95
npopescu <npopescu@localhost>
2020-07-26 19:36:56 -0500

    add o3 flag for compiling benchmarks

--- spawn.sh
+++ spawn.sh
@@ -8,17 +8,41 @@ source ~/.bashrc
 #rustup toolchain link nobc+sl /benchdata/rust/rust-nobc+sl/build/x86_64-unknown-linux-gnu/stage2
 #rustup toolchain link safelib /benchdata/rust/rust-safelib/build/x86_64-unknown-linux-gnu/stage2
 
-OUTNAME="results-bcrm-pass-lto-off"
+#python3 iter_to_bench_n.py
 
-#SUBDIRS="./crates/crates/*"
-#
-#for d in ${SUBDIRS[@]}
-#do
-#	rm -r "$d/$OUTNAME"
-#done
+
+# *****Comp Version #1*****
+
+OUTNAME="results-bcrmpass-embedbitcode-no-lto-off-benchn"
 
 # Pre-compile
 #./bench.sh -c -o "$OUTNAME"
+./pass-bench.sh -c -p -o "$OUTNAME"
 
 # Run
 #./bench.sh -b -r 2 -o "$OUTNAME"
+#./pass-bench.sh -b -r 2 -o "$OUTNAME"
+
+
+# *****Comp Version #2*****
+
+OUTNAME="results-bcrmpass-embedbitcode-no-lto-off-benchn-o3"
+
+# Pre-compile
+./pass-bench.sh -c -p -m -o "$OUTNAME"
+
+# Run
+#./pass-bench.sh -b -r 2 -o "$OUTNAME"
+
+
+# **************************
+
+#SUBDIRS="./crates/crates/*"
+#exp="UNMOD"
+#
+#for d in ${SUBDIRS[@]}
+#do
+#	mkdir -p "$d/$exp/$OUTNAME"
+#	mv "$d/$exp/deps" "$d/$exp/$OUTNAME"
+#done
+

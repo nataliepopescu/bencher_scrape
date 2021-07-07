d38562c9ecd5ebd13a37c0eeeeaad26a50fbebd7
Natalie Popescu <nataliepopescu@gmail.com>
2021-07-07 16:13:15 -0400

    script for cloning eval apps locally

---
+++ clone.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+git clone git@github.com:dropbox/rust-brotli-decompressor.git
+git clone git@github.com:gfx-rs/gfx.git
+git clone git@github.com:tantivy-search/tantivy.git
+git clone git@github.com:cloudflare/quiche.git
+git clone git@github.com:seanmonstar/warp.git
+git clone git@github.com:Schniz/fnm.git
+git clone git@github.com:hyperium/tonic.git
+git clone git@github.com:timberio/vector.git
+git clone git@github.com:google/flatbuffers.git
+git clone git@github.com:firecracker-microvm/firecracker.git
+git clone git@github.com:swc-project/swc.git
+git clone git@github.com:valeriansaliou/sonic.git
+git clone git@github.com:wasmerio/wasmer.git
+git clone git@github.com:tikv/tikv.git
+git clone git@github.com:RustPython/RustPython.git
+git clone git@github.com:diesel-rs/diesel.git
+git clone git@github.com:getzola/zola.git
+git clone git@github.com:iron/iron.git
+git clone git@github.com:str4d/rage.git
+git clone git@github.com:cloudflare/boringtun.git
+git clone git@github.com:BLAKE3-team/BLAKE3.git
+git clone git@github.com:influxdata/flux.git
+git clone git@github.com:utah-scs/splinter.git
+git clone git@github.com:NetSys/NetBricks.git
+git clone git@github.com:servo/servo.git
+git clone git@github.com:mozilla/gecko-dev.git

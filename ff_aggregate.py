0fd9da417fba844fd7a2ec284d2f16af49ef050d
Natalie Popescu <nataliepopescu@gmail.com>
2021-08-12 19:06:54 -0400

    bench ff

--- ff_aggregate.py
+++ ff_aggregate.py
@@ -6,105 +6,137 @@ import subprocess
 import shutil
 import json
 from result_presenter import geomean_overflow
+from statistics import median
 
 def process(root):
+    subdir = ["a11yr", 
+            "about_newtab_with_snippets", 
+            "about_preferences_basic", 
+            "cpstartup",
+            "displaylist_mutate",
+            "glterrain",
+            "pdfpaint",
+            "perf_reftest",
+            "perf_reftest_singletons",
+            "rasterflood_gradient",
+            "rasterflood_svg",
+            "sessionrestore"]
     # each subdirectory in this root dir should be a different set of 
     # tests, w each having its own JSON organization so handle each separately 
-    if os.path.exists(root): 
-        # a11yr
-        bname = "a11yr_keep"
+    for bname in subdir: 
         full_path = os.path.join(root, bname)
-        print()
-        print("BENCHMARK: {}\t".format(bname))
+        print("\n*****BENCHMARK: {}*****".format(bname))
         if os.path.isdir(full_path): 
-            unmod_vals = []
-            regex_vals = []
-            for r in range(32):
-                output = open(os.path.join(full_path, "bench_{}.data".format(r)), 'w')
-                unmod = open(os.path.join(full_path, "unmod_{}.json".format(r)), 'r').read()
-                regex = open(os.path.join(full_path, "regex_{}.json".format(r)), 'r').read()
-                json_unmod = json.loads(unmod)
-                json_regex = json.loads(regex)
-                unmod_val = json_unmod.get("suites")[0].get("subtests")[0].get("value")
-                regex_val = json_regex.get("suites")[0].get("subtests")[0].get("value")
-                unmod_vals.append(unmod_val)
-                regex_vals.append(regex_val)
-            unmod_geomean = geomean_overflow(unmod_vals)
-            regex_geomean = geomean_overflow(regex_vals)
-            print("UNMOD geomean: {}".format(unmod_geomean))
-            print("REGEX geomean: {}".format(regex_geomean))
-            print("REGEX vs UNMOD speedup: {}".format(unmod_geomean/regex_geomean))
+            if bname == "a11yr" or \
+                    bname == "cpstartup" or \
+                    bname == "pdfpaint" or \
+                    bname == "perf_reftest_singletons" or \
+                    bname == "rasterflood_gradient" or \
+                    bname == "rasterflood_svg" or \
+                    bname == "sessionrestore": 
+                unmod_vals = []
+                regex_vals = []
+                speedups = []
+                for r in range(32):
+                    unmod_path = os.path.join(full_path, "unmod_{}.json".format(r))
+                    regex_path = os.path.join(full_path, "regex_{}.json".format(r))
+                    json_unmod = json.loads(open(unmod_path).read())
+                    json_regex = json.loads(open(regex_path).read())
+                    unmod_val = json_unmod.get("suites")[0].get("subtests")[0].get("value")
+                    regex_val = json_regex.get("suites")[0].get("subtests")[0].get("value")
+                    unmod_vals.append(unmod_val)
+                    regex_vals.append(regex_val)
+                    speedups.append(unmod_val/regex_val)
+                unmod_med = median(unmod_vals)
+                regex_med = median(regex_vals)
+                print("speedup of medians: {}".format(unmod_med/regex_med))
+                print("speedup geomean: {}".format(geomean_overflow(speedups)))
 
-        # about_newtab_with_snippets
-        bname = "about_newtab_with_snippets"
-        full_path = os.path.join(root, bname)
-        print()
-        print("BENCHMARK: {}\t".format(bname))
-        if os.path.isdir(full_path): 
-            unmod_vals_1 = []
-            regex_vals_1 = []
-            unmod_vals_2 = []
-            regex_vals_2 = []
-            for r in range(32):
-                output = open(os.path.join(full_path, "bench_{}.data".format(r)), 'w')
-                unmod = open(os.path.join(full_path, "unmod_{}.json".format(r)), 'r').read()
-                regex = open(os.path.join(full_path, "regex_{}.json".format(r)), 'r').read()
-                json_unmod = json.loads(unmod)
-                json_regex = json.loads(regex)
+            if bname == "about_newtab_with_snippets": 
+                unmod_vals_1 = []
+                regex_vals_1 = []
+                unmod_vals_2 = []
+                regex_vals_2 = []
+                speedups_1 = []
+                speedups_2 = []
+                for r in range(32):
+                    unmod_path = os.path.join(full_path, "unmod_{}.json".format(r))
+                    regex_path = os.path.join(full_path, "regex_{}.json".format(r))
+                    json_unmod = json.loads(open(unmod_path).read())
+                    json_regex = json.loads(open(regex_path).read())
+                    # bench 1
+                    unmod_val_1 = json_unmod.get("suites")[0].get("value")
+                    regex_val_1 = json_regex.get("suites")[0].get("value")
+                    unmod_vals_1.append(unmod_val_1)
+                    regex_vals_1.append(regex_val_1)
+                    speedups_1.append(unmod_val_1/regex_val_1)
+                    # bench 2
+                    unmod_val_2 = json_unmod.get("suites")[1].get("subtests")[0].get("value")
+                    regex_val_2 = json_regex.get("suites")[1].get("subtests")[0].get("value")
+                    unmod_vals_2.append(unmod_val_2)
+                    regex_vals_2.append(regex_val_2)
+                    speedups_2.append(unmod_val_2/regex_val_2)
+                unmod_med_1 = median(unmod_vals_1)
+                regex_med_1 = median(regex_vals_1)
+                print("speedup of medians: {}".format(unmod_med_1/regex_med_1))
+                print("speedup geomean: {}".format(geomean_overflow(speedups_1)))
+                unmod_med_2 = median(unmod_vals_2)
+                regex_med_2 = median(regex_vals_2)
+                print("speedup of medians: {}".format(unmod_med_2/regex_med_2))
+                print("speedup geomean: {}".format(geomean_overflow(speedups_2)))
 
-                # bench 1
-                unmod_val_1 = json_unmod.get("suites")[0].get("value")
-                regex_val_1 = json_regex.get("suites")[0].get("value")
-                unmod_vals_1.append(unmod_val_1)
-                regex_vals_1.append(regex_val_1)
+            if bname == "about_preferences_basic" or \
+                    bname == "displaylist_mutate" or \
+                    bname == "glterrain" or\
+                    bname == "perf_reftest":
+                unmod_vals = []
+                regex_vals = []
+                speedups = []
+                for r in range(22):
+                    unmod_path = os.path.join(full_path, "unmod_{}.json".format(r))
+                    regex_path = os.path.join(full_path, "regex_{}.json".format(r))
+                    json_unmod = json.loads(open(unmod_path).read())
+                    json_regex = json.loads(open(regex_path).read())
+                    unmod_val = json_unmod.get("suites")[0].get("value")
+                    regex_val = json_regex.get("suites")[0].get("value")
+                    unmod_vals.append(unmod_val)
+                    regex_vals.append(regex_val)
+                    speedups.append(unmod_val/regex_val)
+                print("unmod_vals...")
+                for uv in unmod_vals:
+                    print(uv)
+                print("regex_vals...")
+                for rv in regex_vals:
+                    print(rv)
+                # remove top and bottom two in each
+                print("removing from unmod...")
+                print("removing:\t{}".format(max(unmod_vals)))
+                unmod_vals.remove(max(unmod_vals))
+                print("removing:\t{}".format(max(unmod_vals)))
+                unmod_vals.remove(max(unmod_vals))
+                print("removing:\t{}".format(min(unmod_vals)))
+                unmod_vals.remove(min(unmod_vals))
+                print("removing:\t{}".format(min(unmod_vals)))
+                unmod_vals.remove(min(unmod_vals))
+                print("removing from regex...")
+                print("removing:\t{}".format(max(regex_vals)))
+                regex_vals.remove(max(regex_vals))
+                print("removing:\t{}".format(max(regex_vals)))
+                regex_vals.remove(max(regex_vals))
+                print("removing:\t{}".format(min(regex_vals)))
+                regex_vals.remove(min(regex_vals))
+                print("removing:\t{}".format(min(regex_vals)))
+                regex_vals.remove(min(regex_vals))
 
-                # bench 2
-                unmod_val_2 = json_unmod.get("suites")[1].get("subtests")[0].get("value")
-                regex_val_2 = json_regex.get("suites")[1].get("subtests")[0].get("value")
-                unmod_vals_2.append(unmod_val_2)
-                regex_vals_2.append(regex_val_2)
-
-            unmod_geomean_1 = geomean_overflow(unmod_vals_1)
-            regex_geomean_1 = geomean_overflow(regex_vals_1)
-            print("UNMOD geomean bench #1: {}".format(unmod_geomean_1))
-            print("REGEX geomean bench #1: {}".format(regex_geomean_1))
-            print("REGEX vs UNMOD speedup: {}".format(unmod_geomean_1/regex_geomean_1))
-
-            unmod_geomean_2 = geomean_overflow(unmod_vals_2)
-            regex_geomean_2 = geomean_overflow(regex_vals_2)
-            print("UNMOD geomean bench #2: {}".format(unmod_geomean_2))
-            print("REGEX geomean bench #2: {}".format(regex_geomean_2))
-            print("REGEX vs UNMOD speedup: {}".format(unmod_geomean_2/regex_geomean_2))
-
-        # about_preferences_basic
-        bname = "about_preferences_basic"
-        full_path = os.path.join(root, bname)
-        print()
-        print("BENCHMARK: {}\t".format(bname))
-        if os.path.isdir(full_path): 
-            unmod_vals = []
-            regex_vals = []
-            for r in range(32):
-                output = open(os.path.join(full_path, "bench_{}.data".format(r)), 'w')
-                unmod = open(os.path.join(full_path, "unmod_{}.json".format(r)), 'r').read()
-                regex = open(os.path.join(full_path, "regex_{}.json".format(r)), 'r').read()
-                json_unmod = json.loads(unmod)
-                json_regex = json.loads(regex)
-
-                unmod_val = json_unmod.get("suites")[0].get("value")
-                regex_val = json_regex.get("suites")[0].get("value")
-                unmod_vals.append(unmod_val)
-                regex_vals.append(regex_val)
-            unmod_geomean = geomean_overflow(unmod_vals)
-            regex_geomean = geomean_overflow(regex_vals)
-            print("UNMOD geomean: {}".format(unmod_geomean))
-            print("REGEX geomean: {}".format(regex_geomean))
-            print("REGEX vs UNMOD speedup: {}".format(unmod_geomean/regex_geomean))
+                unmod_med = median(unmod_vals)
+                regex_med = median(regex_vals)
+                print("speedup of medians: {}".format(unmod_med/regex_med))
+                print("speedup geomean: {}".format(geomean_overflow(speedups)))
 
 def arg_parse():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--root", 
-            metavar="path/to/deps/",
+    parser.add_argument("--root", "-r", 
+            metavar="path/to/results/",
             type=str,
             required=True,
             help="path to the directory containing Firefox results")
--- ff_bench.sh
+++ ff_bench.sh
@@ -7,6 +7,9 @@
 script_path=`realpath $0`
 root_path=`dirname $script_path`
 
+export RUSTUP_TOOLCHAIN=rust-mir-mod
+export RUSTFLAGS="-Z convert-unchecked-indexing"
+
 ffdir="ff_results"
 mkdir -p $ffdir
 mkdir -p $ffdir/$1
@@ -15,6 +18,9 @@ outfile="testing/mozharness/build/local.json"
 rounds=$(($2-1))
 echo $rounds
 
+echo "currently running:"
+echo $1
+
 # run each unmod/regex benchmark multiple times
 for num in $(seq 0 $rounds)
 do
@@ -22,20 +28,24 @@ do
 if [ $(($num%2)) -eq 0 ]; then
 
 	echo ""
+	echo "round $num"
+	echo ""
 	echo "even!"
 	echo "running regex first"
 	echo ""
 	# run regex
-	cd ../gecko_regex
-	unshare -rn xvfb-run ./mach talos-test --activeTests $1
+	cd ../regex-mozilla-central
+	xvfb-run ./mach talos-test --activeTests $1
+	#unshare -rn xvfb-run ./mach talos-test --activeTests $1
 	cp $outfile $root_path/$ffdir/$1/regex_$num.json
 	rm $outfile
 	# run unmod
 	echo ""
 	echo "running unmod second"
 	echo ""
-	cd ../gecko
-	unshare -rn xvfb-run ./mach talos-test --activeTests $1
+	cd ../mozilla-central
+	xvfb-run ./mach talos-test --activeTests $1
+	#unshare -rn xvfb-run ./mach talos-test --activeTests $1
 	cp $outfile $root_path/$ffdir/$1/unmod_$num.json
 	rm $outfile
 
@@ -43,20 +53,24 @@ if [ $(($num%2)) -eq 0 ]; then
 else
 
 	echo ""
+	echo "round $num"
+	echo ""
 	echo "odd!"
 	echo "running unmod first"
 	echo ""
 	# run unmod
-	cd ../gecko
-	unshare -rn xvfb-run ./mach talos-test --activeTests $1
+	cd ../mozilla-central
+	xvfb-run ./mach talos-test --activeTests $1
+	#unshare -rn xvfb-run ./mach talos-test --activeTests $1
 	cp $outfile $root_path/$ffdir/$1/unmod_$num.json
 	rm $outfile
 	# run regex
 	echo ""
 	echo "running regex second"
 	echo ""
-	cd ../gecko_regex
-	unshare -rn xvfb-run ./mach talos-test --activeTests $1
+	cd ../regex-mozilla-central
+	xvfb-run ./mach talos-test --activeTests $1
+	#unshare -rn xvfb-run ./mach talos-test --activeTests $1
 	cp $outfile $root_path/$ffdir/$1/regex_$num.json
 	rm $outfile
 
